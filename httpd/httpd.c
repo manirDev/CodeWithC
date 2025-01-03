d0214cce61f7eb03ba0b7f54c995f57e65a88c31
manirdev <mhtnourmhtmjr@gmail.com>
2025-01-04 00:01:03 +0300

    httpd file is modified

--- httpd/httpd.c
+++ httpd/httpd.c
@@ -7,8 +7,41 @@
 #include "netinet/in.h"
 #include <arpa/inet.h>
 
+#define LISTENADDR "0.0.0.0"
 
-int main()
+int initServer(int portNumber)
 {
+    int socketRet;
+    struct sockaddr_in server;
+
+    socketRet = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketRet < 0)
+    {
+        return 0;
+    }
+
+    server.sin_addr.s_addr = inet_addr(LISTENADDR);
+    server.sin_family = AF_INET6;
+    server.sin_port = htons(portNumber);
+    
+}
+
+int main(int argc, char *argv[])
+{
+    int retServer;
+    char* port;
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <listening port>\n", argv[0]);
+        return -1;
+    }
+    else
+    {
+        port = argv[1];
+    }
+
+
+    retServer = initServer(atoi(port));
+
     return 0;
 }
