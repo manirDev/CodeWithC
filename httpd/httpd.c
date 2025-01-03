99073c05b6bf0fe76746195acb3daa1f3a0d20e8
manirdev <mhtnourmhtmjr@gmail.com>
2025-01-04 00:18:04 +0300

    httpd int_server function is implemented.

--- httpd/httpd.c
+++ httpd/httpd.c
@@ -9,21 +9,39 @@
 
 #define LISTENADDR "0.0.0.0"
 
+char* error;
+
 int initServer(int portNumber)
 {
-    int socketRet;
+    int socketFd;
     struct sockaddr_in server;
 
-    socketRet = socket(AF_INET, SOCK_STREAM, 0);
-    if (socketRet < 0)
+    socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFd < 0)
     {
+        error = "scoket() errro";
         return 0;
     }
 
     server.sin_addr.s_addr = inet_addr(LISTENADDR);
     server.sin_family = AF_INET6;
     server.sin_port = htons(portNumber);
+
+    if (bind(socketFd, (struct socketaddr*)&server, sizeof(server)))
+    {
+        error = "bind() eroor";
+        close(socketFd);
+        return 0;
+    }
     
+    if (listen(socketFd, 5))
+    {
+        error = "listen() error";
+        close(socketFd);
+        return 0;
+    }
+    
+    return socketFd;
 }
 
 int main(int argc, char *argv[])
@@ -42,6 +60,12 @@ int main(int argc, char *argv[])
 
 
     retServer = initServer(atoi(port));
+    if (!retServer)
+    {
+        fprintf(stderr, "%s\n", error);
+        return -1;
+    }
+    
 
     return 0;
 }
